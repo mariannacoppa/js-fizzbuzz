283551f48f370d2a7b1e8c6e661be79d43ff1fb9
mariannacoppa <densimetra@gmail.com>
2024-06-27 16:14:43 +0200

    [UPD] add instruction for variables multiple of 5

--- js/script.js
+++ js/script.js
@@ -3,5 +3,8 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 != 0) {
         console.log(`${i} Fizz`);
     }
+    else if (i % 5 == 0 && i % 3 != 0) {
+        console.log(`${i} Buzz`);
+    }
     
 }
